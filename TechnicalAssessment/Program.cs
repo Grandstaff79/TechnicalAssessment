b673a703b072799fba227da2d913f58e482a429c
Grandstaff79 <48068304+Grandstaff79@users.noreply.github.com>
2022-02-13 18:09:37 -0500

    Add project files.

---
+++ TechnicalAssessment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TechnicalAssessment
+{
+    internal class Program
+    {
+     static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please input file name: ");
+            Console.ReadLine();
+
+            DirectoryInfo place = new DirectoryInfo(@"C:\Users\TravisGrandstaff\Desktop\Emails\names.csv");
+            FileInfo[] Files = place.GetFiles();
+
+            foreach (FileInfo i in Files)
+            {
+                Console.WriteLine("File Name - {0}", i.Name);
+            }
+           
+
+
+            
+        }
+    }
+}
