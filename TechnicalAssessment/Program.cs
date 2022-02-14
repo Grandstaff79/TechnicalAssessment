2707daaf035d1cd274df81356ae3b212c1053e1a
Grandstaff79 <48068304+Grandstaff79@users.noreply.github.com>
2022-02-13 20:29:04 -0500

    added names.csv

--- TechnicalAssessment/Program.cs
+++ TechnicalAssessment/Program.cs
@@ -4,28 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data;
+using System.Reflection;
+using LumenWorks.Framework.IO.Csv;
+
 
 namespace TechnicalAssessment
 {
     internal class Program
     {
-     static void Main(string[] args)
+         static void Main()
         {
-           
+
+            var csvTable = new DataTable();
+            using (var csvReader = new CsvReader(new StreamReader(File.OpenRead(@"C:\Users\TravisGrandstaff\Desktop\rawCSV\names.csv")), true))
+            {
+                csvTable.Load(csvReader);
+            }
+
+
+            if (File.Exists("names.csv"))
+            {
+                Console.WriteLine("The file exists.");
+            }
+            if (File.Exists(@"C:\names.csv"))
+            {
+                Console.WriteLine("The file exists.");
+            }
+
             Console.WriteLine("Please input file name: ");
             Console.ReadLine();
-
-            DirectoryInfo place = new DirectoryInfo(@"C:\Users\TravisGrandstaff\Desktop\Emails\names.csv");
-            FileInfo[] Files = place.GetFiles();
-
-            foreach (FileInfo i in Files)
-            {
-                Console.WriteLine("File Name - {0}", i.Name);
-            }
-           
+        }
 
 
-            
+
+    public class SearchParameters
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+
         }
     }
 }
+
+        
+
+     
+
+
+            
+                
+   
+        
+    
+
