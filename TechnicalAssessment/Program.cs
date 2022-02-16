4ed809f60e97ee66c8b3411c84c14eb0891330bb
Grandstaff79 <48068304+Grandstaff79@users.noreply.github.com>
2022-02-16 16:40:22 -0500

    added search directory for .csv file and parse .csv file code

--- TechnicalAssessment/Program.cs
+++ TechnicalAssessment/Program.cs
@@ -13,14 +13,76 @@ namespace TechnicalAssessment
 {
     internal class Program
     {
-         static void Main()
+         static void Main(string[] args)
         {
+             Console.WriteLine("Please input file name: ");
+            Console.ReadLine();
+        }
+    }
+  
+}
+        public class TextFieldParser : IDisposable {} 
+                
+            using (TextFieldParser parser = new TextFieldParser(@"C:\Users\TravisGrandstaff\Desktop\rawCSV\names.csv"))
+  {
+    parser.TextFieldType = FieldType.Delimited;
+    parser.SetDelimiters(",");
+    while (!parser.EndOfData)
+    {
+        //Process row
+        string[] fields = parser.ReadFields();
+        foreach (string field in fields)
+        {
+            //TODO: Process field
+        }
+    }
+}
+            
 
             var csvTable = new DataTable();
             using (var csvReader = new CsvReader(new StreamReader(File.OpenRead(@"C:\Users\TravisGrandstaff\Desktop\rawCSV\names.csv")), true))
             {
                 csvTable.Load(csvReader);
             }
+
+             void Page_Load(object sender, EventArgs e)
+{
+    SearchFile("prism");
+}
+
+
+
+ 
+     void SearchFile(string fileName)
+{
+    DriveInfo[] allDrives = DriveInfo.GetDrives();
+    List<string> files = new List<string>();
+    foreach (DriveInfo dir in allDrives)
+    {
+        if (dir.IsReady)
+        {
+            DirSearch(dir.Name, fileName, files);
+        }
+    }
+}
+              void DirSearch(string dirctoryName, string fileName, List<string> files);
+
+    try
+    {      
+        foreach (string d in Directory.GetDirectories(dirctoryName))
+        {
+            foreach (string f in Directory.GetFiles(d, "*" + fileName + "*.*"))
+            {
+               files.Add(f);
+            }
+            DirSearch(d, fileName, files);
+        }
+    }
+    catch (System.Exception excpt)
+    {
+        Response.Write(excpt.Message);
+    }
+
 
 
             if (File.Exists("names.csv"))
@@ -32,11 +94,9 @@ namespace TechnicalAssessment
                 Console.WriteLine("The file exists.");
             }
 
-            Console.WriteLine("Please input file name: ");
-            Console.ReadLine();
-        }
+           
 
-
+      
 
     public class SearchParameters
         {
@@ -45,8 +105,10 @@ namespace TechnicalAssessment
             public string Email { get; set; }
 
         }
-    }
-}
+    
+
+   
+
 
         
 
